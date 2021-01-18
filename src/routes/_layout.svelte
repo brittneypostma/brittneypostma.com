30e0879f344edfe5ac974203618a6865f2c0c13a
brittneypostma <brittney.postma@hotmail.com>
2021-01-17 22:11:14 -0500

    fixed mobile scroll

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -11,6 +11,111 @@
   onMount(() => (ready = true))
 </script>
 
+<Header />
+<Wallpaper />
+<main>
+  {#if ready}
+    <div class="container">
+      <a
+        href="https://www.bdesigned.dev"
+        class="bubble"
+        transition:fly="{{
+          duration: 1000,
+          delay: 100,
+          easing: quintOut,
+          y: 150,
+        }}">
+        <img src="bdesigned.svg" alt="bDesigned logo" class="logos" />
+        <h2 class="title">bDesigned</h2>
+        <p class="text">
+          Brittney runs her own freelance business, bDesigned. See some of her
+          projects or contact her about work.
+        </p>
+      </a>
+      <a
+        href="https://www.theconsolelogs.com"
+        class="bubble"
+        transition:fly="{{
+          duration: 1000,
+          delay: 400,
+          easing: quintOut,
+          y: 150,
+        }}">
+        <img src="logs.png" alt="The Console Logs logo" class="logos" />
+        <h2 class="title">The Console Logs</h2>
+        <p class="text">
+          Brittney writes notes on a number of development topics at The Console
+          Logs. Read up on topics from html to advanced javascript and react.
+        </p>
+      </a>
+      <a
+        href="https://academy.zerotomastery.io/courses/workshops/lectures/26936743?affcode=441520_gjue7n-1"
+        class="bubble"
+        transition:fly="{{
+          duration: 1000,
+          delay: 800,
+          easing: quintOut,
+          y: 50,
+        }}">
+        <img src="ztm.png" alt="ZTM Academy" class="logos" />
+        <h2 class="title">ZTM Academy</h2>
+        <p class="text">
+          Brittney is an insructor over at the ZTM Academy. Watch her workshop
+          on Responsive Web Design.
+        </p>
+      </a>
+      <a
+        href="https://www.redbubble.com/people/bDesigned/shop"
+        class="bubble"
+        transition:fly="{{
+          duration: 1000,
+          delay: 800,
+          easing: quintOut,
+          y: 50,
+        }}">
+        <img
+          src="corgi-star.png"
+          alt="Party Corgi Star"
+          class="logos"
+          id="corgi"
+        />
+        <h2 class="title">RedBubble Store</h2>
+        <p class="text">
+          Brittney has a store on RedBubble for Party Corgi and other content
+          created in Figma.
+        </p>
+      </a>
+    </div>
+
+    <div class="beach" transition:fade="{{ duration: 1000 }}">
+      <svg
+        width="1440"
+        height="813"
+        viewBox="0 0 1440 813"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M1289.5 177C1394 83 1344 43.5 1440 0.5V813H357.325C182.558 834.169 -4.5 813 -4.5 813H140H357.325C456.472 800.991 551.663 775.356 606.5 724.5C681 655.409 771.017 613.01 840.5 573.5C942.5 515.5 981 410.5 1069 339C1144.03 278.04 1246.71 215.49 1289.5 177Z"
+          fill="url(#paint0_linear)"
+        ></path>
+        <defs>
+          <linearGradient
+            id="paint0_linear"
+            x1="1440"
+            y1="799.001"
+            x2="1080"
+            y2="380.5"
+            gradientUnits="userSpaceOnUse">
+            <stop stop-color="#D59B69"></stop>
+            <stop offset="1" stop-color="#DDB184"></stop>
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  {/if}
+  <Footer />
+</main>
+
 <style>
   .beach {
     filter: drop-shadow(-4px -8px 8px rgba(3, 3, 3, 0.25));
@@ -49,12 +154,11 @@
   }
 
   .container {
-    height: calc(100% - 83px);
     max-width: 1440px;
-    margin: 2.5rem auto;
+    margin: 5rem auto;
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
-    gap: 4rem;
+    gap: 5rem;
     place-content: center;
     place-items: center;
   }
@@ -80,6 +184,14 @@
     width: 125px;
   }
 
+  @media (min-width: 1200px) {
+    .container {
+      height: calc(100% - 83px);
+      margin: 2.5rem auto;
+      gap: 4rem;
+    }
+  }
+
   /* @media (min-width: 1200px) {
     .container {
       grid-template: 150px 150px 150px / 1fr 1fr 1fr;
@@ -100,86 +212,3 @@
     }
   }
 </style>
-
-<Header />
-<Wallpaper />
-<main>
-  {#if ready}
-    <div class="container">
-      <a
-        href="https://www.bdesigned.dev"
-        class="bubble"
-        transition:fly={{ duration: 1000, delay: 100, easing: quintOut, y: 150 }}>
-        <img src="logo.png" alt="bDesigned logo" class="logos" />
-        <h2 class="title">bDesigned</h2>
-        <p class="text">
-          Brittney runs her own freelance business, bDesigned. See some of her
-          projects or contact her about work.
-        </p>
-      </a>
-      <a
-        href="https://www.theconsolelogs.com"
-        class="bubble"
-        transition:fly={{ duration: 1000, delay: 400, easing: quintOut, y: 150 }}>
-        <img src="logs.png" alt="The Console Logs logo" class="logos" />
-        <h2 class="title">The Console Logs</h2>
-        <p class="text">
-          Brittney writes notes on a number of development topics at The Console
-          Logs. Read up on topics from html to advanced javascript and react.
-        </p>
-      </a>
-      <a
-        href="https://academy.zerotomastery.io/courses/workshops/lectures/26936743?affcode=441520_gjue7n-1"
-        class="bubble"
-        transition:fly={{ duration: 1000, delay: 800, easing: quintOut, y: 50 }}>
-        <img src="ztm.png" alt="ZTM Academy" class="logos" />
-        <h2 class="title">ZTM Academy</h2>
-        <p class="text">
-          Brittney is an insructor over at the ZTM Academy. Watch her workshop
-          on Responsive Web Design.
-        </p>
-      </a>
-      <a
-        href="https://www.redbubble.com/people/bDesigned/shop"
-        class="bubble"
-        transition:fly={{ duration: 1000, delay: 800, easing: quintOut, y: 50 }}>
-        <img
-          src="corgi-star.png"
-          alt="Party Corgi Star"
-          class="logos"
-          id="corgi" />
-        <h2 class="title">RedBubble Store</h2>
-        <p class="text">
-          Brittney has a store on RedBubble for Party Corgi and other content
-          created in Figma.
-        </p>
-      </a>
-    </div>
-
-    <div class="beach" transition:fade={{ duration: 1000 }}>
-      <svg
-        width="1440"
-        height="813"
-        viewBox="0 0 1440 813"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M1289.5 177C1394 83 1344 43.5 1440 0.5V813H357.325C182.558 834.169 -4.5 813 -4.5 813H140H357.325C456.472 800.991 551.663 775.356 606.5 724.5C681 655.409 771.017 613.01 840.5 573.5C942.5 515.5 981 410.5 1069 339C1144.03 278.04 1246.71 215.49 1289.5 177Z"
-          fill="url(#paint0_linear)" />
-        <defs>
-          <linearGradient
-            id="paint0_linear"
-            x1="1440"
-            y1="799.001"
-            x2="1080"
-            y2="380.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#D59B69" />
-            <stop offset="1" stop-color="#DDB184" />
-          </linearGradient>
-        </defs>
-      </svg>
-    </div>
-  {/if}
-  <Footer />
-</main>
