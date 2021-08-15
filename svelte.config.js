597b5d5c0de0aad44be6a6330163d6105794c926
brittneypostma <brittney.postma@hotmail.com>
2021-08-15 12:26:03 -0400

    updated svelte config to use netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /* https://kit.svelte.dev/docs#configuration */
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
