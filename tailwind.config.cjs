f537cf713cc4163daf7ad3855b16dd7a9ca5203c
Brittney Postma <bdesignedwebdev@gmail.com>
2022-12-09 22:25:51 +0000

    skeleton theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,20 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+  ],
+  darkMode:'class',
 
   // theme: {
   //   extend: {},
   // },
 
-  plugins: [],
+  plugins: [
+    require('@skeletonlabs/skeleton/tailwind/theme.cjs'),
+    require('@tailwindcss/typography'),
+  ],
   presets: [require('./theme.cjs')]
 }
 
 module.exports = config
+      
